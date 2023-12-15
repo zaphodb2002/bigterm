7eec2e46bc4ab70e38456d40c297313f66d07301
Joshua Lindhardt <zaphodb2002@gmail.com>
2023-12-15 10:16:17 -0800

    separate functions

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,84 @@
-use crossterm::event::{Event, KeyCode, KeyEventKind};
-use crossterm::terminal::{
-    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+use anyhow::{Error, Result};
+use crossterm::{
+    event::{
+        self,
+        Event::{self, Key},
+        KeyCode::{self, Char},
+        KeyEventKind,
+    },
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
 };
-use crossterm::{event, ExecutableCommand};
-use ratatui::prelude::*;
-use ratatui::widgets::*;
-use std::io::{stdout, Error};
+use ratatui::{
+    prelude::{CrosstermBackend, Frame, Terminal},
+    style::Stylize,
+    widgets::*,
+};
+
+use std::io::stdout;
 
 fn main() -> Result<(), Error> {
-    stdout().execute(EnterAlternateScreen)?;
+    startup()?;
+    let status = run();
+    shutdown()?;
+    status?;
+    Ok(())
+}
+
+fn startup() -> Result<()> {
     enable_raw_mode()?;
+    execute!(std::io::stderr(), EnterAlternateScreen)?;
+    Ok(())
+}
 
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-    terminal.clear()?;
+fn shutdown() -> Result<()> {
+    disable_raw_mode()?;
+    execute!(std::io::stderr(), LeaveAlternateScreen)?;
+    Ok(())
+}
 
-    loop {
-        terminal.draw(|frame| {
-            let area = frame.size();
-            frame.render_widget(Paragraph::new("Hello world!").white().on_blue(), area);
-        })?;
+fn ui(app: &App, f: &mut Frame) {
+    f.render_widget(Paragraph::new(format!("TEST {} TEST", app.text)), f.size());
+}
 
-        if event::poll(std::time::Duration::from_millis(16))? {
-            if let Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                    break;
+fn update(app: &mut App) -> Result<()> {
+    if event::poll(std::time::Duration::from_millis(250))? {
+        if let Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press {
+                match key.code {
+                    Char('q') => app.should_quit = true,
+                    _ => {}
                 }
             }
         }
     }
-
-    stdout().execute(LeaveAlternateScreen)?;
-    disable_raw_mode()?;
     Ok(())
+}
+
+fn run() -> Result<()> {
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    let mut app = App {
+        text: "This is from the app!".to_string(),
+        should_quit: false,
+    };
+
+    loop {
+        terminal.draw(|f| {
+            ui(&app, f);
+        })?;
+
+        update(&mut app)?;
+
+        if app.should_quit {
+            break;
+        }
+    }
+    Ok(())
+}
+
+struct App {
+    text: String,
+    should_quit: bool,
 }
